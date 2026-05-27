a60236e475daa06bb15558d249d49476706eaea5
AryanP-10 <aryanparadkar10@gmail.com>
2026-05-27 12:32:37 +0530

    Added ApplyRequest DTO

---
+++ src/main/java/com/example/dto/ApplyRequest.java
@@ -0,0 +1,26 @@
+package com.example.dto;
+
+public class ApplyRequest {
+
+	private int studentId;
+
+	private int jobId;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+
+		this.studentId = studentId;
+	}
+
+	public int getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(int jobId) {
+
+		this.jobId = jobId;
+	}
+}
